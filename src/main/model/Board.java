3fcda6335bea3c27e6f7937550d58286248a9211
Adnan Hasnat <u7300623@anu.edu.au>
2026-05-26 11:57:30 +1000

    Knight and King logic

--- src/main/model/Board.java
+++ src/main/model/Board.java
@@ -1,10 +1,13 @@
 import java.lang.Math;
+import java.util.ArrayList;
 
 public class Board {
     // for 91 space board; there are 11 files going vertically and 11 ranks going skew-horizontally.
     // for files on the outer edges of the board, some of the ranks don't exist, e.g. only a1-6 exist
     // f, or file 6, is the middle of the board
     Piece[][] board = new Piece[11][11];
+
+    public static final int centerFile = 5;
 
     public static class Position {
         // these are values from 0 to 10 inclusive
@@ -24,6 +27,13 @@ public class Board {
             this.file = (int) ((pos.toLowerCase().charAt(0) - 'a'));
             this.rank = Integer.parseInt(pos.substring(1)) - 1;
         }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other instanceof Position) {
+                return this.file == ((Position) other).file && this.rank == ((Position) other).rank;
+            } else return false;
+        }
     }
 
     public static class Move {
@@ -34,9 +44,15 @@ public class Board {
             this.fromPos = fromPos;
             this.toPos = toPos;
         }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other instanceof Move) {
+                return this.fromPos.equals(((Move) other).fromPos) && this.toPos.equals(((Move) other).toPos);
+            } else return false;
+        }
     }
 
-    // Note: g
     public Piece getPos(Position pos) {
         return this.board[pos.file][pos.rank];
     }
@@ -99,8 +115,8 @@ public class Board {
                 }
 
                 int xBoard = x/xScale;
-                float yBoard = ((float) (y - Math.abs(xBoard - 5)))/2;
-                if (yBoard < 0 || yBoard >= 11 - Math.abs(xBoard - 5)) {
+                float yBoard = ((float) (y - Math.abs(xBoard - centerFile)))/2;
+                if (yBoard < 0 || yBoard >= 11 - Math.abs(xBoard - centerFile)) {
                     string.append(' ');
                     continue;
                 }
@@ -140,62 +156,183 @@ public class Board {
 
     public boolean isInBounds(Position pos) {
         boolean isFileInBounds = pos.file >= 0 && pos.file < 11;
-        boolean isRankInBounds = pos.rank >= 0 && pos.rank < 11 - Math.abs(pos.file - 5);
+        boolean isRankInBounds = pos.rank >= 0 && pos.rank < 11 - Math.abs(pos.file - centerFile);
         return isFileInBounds && isRankInBounds;
     }
 
     public static int distanceFromCenter(Position pos) {
-        return Math.abs(pos.file - 5);
+        return Math.abs(pos.file - centerFile);
     }
+
+    public Position oneStepForward(Position pos, Piece.Color playerColor) {
+        int forwardStep = playerColor == Piece.Color.WHITE ? 1 : -1;
+
+        return new Position(pos.file, pos.rank + forwardStep);
+    }
+
+    public Position oneStepBackward(Position pos, Piece.Color playerColor) {
+        int forwardStep = playerColor == Piece.Color.WHITE ? 1 : -1;
+
+        return new Position(pos.file, pos.rank - forwardStep);
+    }
+
+    public Position oneStepLeftAndForward(Position pos, Piece.Color playerColor) {
+        int forwardStep = playerColor == Piece.Color.WHITE ? 1 : -1;
+
+        if (pos.file < centerFile) {
+            return new Position(pos.file - 1, pos.rank);
+        } else {
+            return new Position(pos.file - 1, pos.rank + forwardStep);
+        }
+    }
+
+    public Position oneStepRightAndForward(Position pos, Piece.Color playerColor) {
+        int forwardStep = playerColor == Piece.Color.WHITE ? 1 : -1;
+
+        if (pos.file < centerFile) {
+            return new Position(pos.file + 1, pos.rank + forwardStep);
+        } else {
+            return new Position(pos.file + 1, pos.rank);
+        }
+    }
+
+    public Position oneStepLeftAndBackward(Position pos, Piece.Color playerColor) {
+        int forwardStep = playerColor == Piece.Color.WHITE ? 1 : -1;
+
+        if (pos.file < centerFile) {
+            return new Position(pos.file - 1, pos.rank - forwardStep);
+        } else {
+            return new Position(pos.file - 1, pos.rank);
+        }
+    }
+    public Position oneStepRightAndBackward(Position pos, Piece.Color playerColor) {
+        int forwardStep = playerColor == Piece.Color.WHITE ? 1 : -1;
+
+        if (pos.file < centerFile) {
+            return new Position(pos.file - 1, pos.rank);
+        } else {
+            return new Position(pos.file - 1, pos.rank - forwardStep);
+        }
+    }
+
+    public Position bishopStepLeft(Position pos, Piece.Color playerColor) {
+        int forwardStep = playerColor == Piece.Color.WHITE ? 1 : -1;
+
+        if (pos.file < centerFile) {
+            return new Position(pos.file - 2, pos.rank - forwardStep);
+        } else {
+            return new Position(pos.file - 2, pos.rank + forwardStep);
+        }
+    }
+
+    public Position bishopStepRight(Position pos, Piece.Color playerColor) {
+        int forwardStep = playerColor == Piece.Color.WHITE ? 1 : -1;
+
+        if (pos.file < centerFile) {
+            return new Position(pos.file + 2, pos.rank + forwardStep);
+        } else {
+            return new Position(pos.file + 2, pos.rank - forwardStep);
+        }
+    }
+
+    public Position bishopStepForwardLeft(Position pos, Piece.Color playerColor) {
+        int forwardStep = playerColor == Piece.Color.WHITE ? 1 : -1;
+
+        if (pos.file < centerFile) {
+            return new Position(pos.file - 1, pos.rank + forwardStep);
+        } else {
+            return new Position(pos.file - 1, pos.rank + 2 * forwardStep);
+        }
+    }
+
+    public Position bishopStepForwardRight(Position pos, Piece.Color playerColor) {
+        int forwardStep = playerColor == Piece.Color.WHITE ? 1 : -1;
+
+        if (pos.file < centerFile) {
+            return new Position(pos.file + 1, pos.rank + 2 * forwardStep);
+        } else {
+            return new Position(pos.file + 1, pos.rank + forwardStep);
+        }
+    }
+
+    public Position bishopStepBackwardLeft(Position pos, Piece.Color playerColor) {
+        int forwardStep = playerColor == Piece.Color.WHITE ? 1 : -1;
+
+        if (pos.file < centerFile) {
+            return new Position(pos.file - 1, pos.rank - 2 * forwardStep);
+        } else {
+            return new Position(pos.file - 1, pos.rank - forwardStep);
+        }
+    }
+    public Position bishopStepBackwardRight(Position pos, Piece.Color playerColor) {
+        int forwardStep = playerColor == Piece.Color.WHITE ? 1 : -1;
+
+        if (pos.file < centerFile) {
+            return new Position(pos.file + 1, pos.rank - forwardStep);
+        } else {
+            return new Position(pos.file + 1, pos.rank - 2 * forwardStep);
+        }
+    }
+
 
     public boolean isLegalMove(Move move, Piece.Color playerColor) {
         if (!isInBounds(move.fromPos) || !isInBounds(move.toPos)) {
             return false;
         }
 
-        Piece piece = this.getPos(move.fromPos);
+        return getLegalMoves(move.fromPos, playerColor).contains(move);
+    }
+
+    /**
+     * Main function: get all legal moves for player playerColor for the piece at fromPos
+     *
+     * @param fromPos
+     * @param playerColor
+     * @return
+     */
+    public ArrayList<Move> getLegalMoves(Position fromPos, Piece.Color playerColor) {
+        Piece piece = this.getPos(fromPos);
+
+        ArrayList<Move> moves = new ArrayList<>();
 
         if (piece == null || piece.color != playerColor) {
             // there is no piece there OR the piece is not the player's color
-            return false;
+            return moves;
         }
 
-        Piece destinationPiece = this.getPos(move.toPos);
-
-        if (destinationPiece != null && destinationPiece.color == playerColor) {
-            // cannot move a piece onto one of your own pieces
-            return false;
-        }
-
-        Position fromPos = move.fromPos;
-        Position toPos = move.toPos;
-
+        ArrayList<Move> possibleMoves;
         switch (piece.type) {
             case PAWN:
                 int forwardStep = playerColor == Piece.Color.WHITE ? 1 : -1;
 
                 // Pawn basic logic: able to move one space ahead into an empty square
-                if (fromPos.file == toPos.file && fromPos.rank + forwardStep == toPos.rank && destinationPiece == null) {
-                    return true;
-                }
+                Position forwardPos = oneStepForward(fromPos, playerColor);
+                if (isInBounds(forwardPos) && this.getPos(forwardPos) == null)
+                    moves.add(new Move(fromPos, forwardPos));
 
                 // Pawn basic logic: able to capture to the side
                 // Note: on hex chess, capturing inwards moves the piece up a rank
                 // and capturing outwards keeps the piece in the same rank
-                if ((fromPos.file == toPos.file - 1 || fromPos.file == toPos.file + 1)      // check for sideways: in neighbouring files
-                     && (distanceFromCenter(toPos) > distanceFromCenter(fromPos)
-                         ? toPos.rank == fromPos.rank
-                         : toPos.rank == fromPos.rank + 1)                                  // check that rank change is correct
-                     && destinationPiece != null) { // check that it's a capturing move
-                        return true;
-                     }
+                Position leftCapturePos = oneStepLeftAndForward(fromPos, playerColor);
+                if (isInBounds(leftCapturePos)
+                        && this.getPos(leftCapturePos) != null
+                        && this.getPos(leftCapturePos).color != playerColor)
+                    moves.add(new Move(fromPos, leftCapturePos));
+
+                Position rightCapturePos = oneStepRightAndForward(fromPos, playerColor);
+                if (isInBounds(rightCapturePos)
+                        && this.getPos(rightCapturePos) != null
+                        && this.getPos(rightCapturePos).color != playerColor)
+                    moves.add(new Move(fromPos, rightCapturePos));
+
 
                 // Pawn logic: if the piece is on a starting position, it can jump two steps forward
                 // Note this is true also if the pawn moves into the starting position of another pawn
                 if (((playerColor == Piece.Color.WHITE && fromPos.rank == 4 - distanceFromCenter(fromPos))
-                    || (playerColor == Piece.Color.BLACK && fromPos.rank == 6)) &&
-                    toPos.file == fromPos.file && toPos.rank == fromPos.rank + 2 * forwardStep) {
-                    return true;
+                        || (playerColor == Piece.Color.BLACK && fromPos.rank == 6))) {
+                    Position doubleForwardPos = new Position(fromPos.file, fromPos.rank + 2 * forwardStep);
+                    if (isInBounds(doubleForwardPos) && this.getPos(doubleForwardPos) == null)
+                        moves.add(new Move(fromPos, doubleForwardPos));
                 }
 
                 // Pawn logic: en passant
@@ -204,18 +341,79 @@ public class Board {
                 // TODO: add
                 break;
             case ROOK:
+                // rook logic: can attack backward and forward up until it reaches a piece or the edge of the board
+
                 break;
             case BISHOP:
                 break;
             case KNIGHT:
-                break;
+                possibleMoves = new ArrayList<>(12);
+
+                possibleMoves.add(new Move(fromPos, oneStepLeftAndForward(
+                                                    oneStepForward(
+                                                    oneStepForward(fromPos, playerColor), playerColor), playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepRightAndForward(
+                                                    oneStepForward(
+                                                    oneStepForward(fromPos, playerColor), playerColor), playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepLeftAndBackward(
+                                                    oneStepLeftAndForward(
+                                                    oneStepLeftAndForward(fromPos, playerColor), playerColor), playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepForward(
+                                                    oneStepLeftAndForward(
+                                                    oneStepLeftAndForward(fromPos, playerColor), playerColor), playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepRightAndBackward(
+                                                    oneStepRightAndForward(
+                                                    oneStepRightAndForward(fromPos, playerColor), playerColor), playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepForward(
+                                                    oneStepRightAndForward(
+                                                    oneStepRightAndForward(fromPos, playerColor), playerColor), playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepLeftAndForward(
+                                                    oneStepLeftAndBackward(
+                                                    oneStepLeftAndBackward(fromPos, playerColor), playerColor), playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepBackward(
+                                                    oneStepLeftAndBackward(
+                                                    oneStepLeftAndBackward(fromPos, playerColor), playerColor), playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepRightAndForward(
+                                                    oneStepRightAndBackward(
+                                                    oneStepRightAndBackward(fromPos, playerColor), playerColor), playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepBackward(
+                                                    oneStepRightAndBackward(
+                                                    oneStepRightAndBackward(fromPos, playerColor), playerColor), playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepLeftAndBackward(
+                                                    oneStepBackward(
+                                                    oneStepBackward(fromPos, playerColor), playerColor), playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepRightAndBackward(
+                                                    oneStepBackward(
+                                                    oneStepBackward(fromPos, playerColor), playerColor), playerColor)));
+
+
+                moves.addAll(possibleMoves.stream().filter(move -> isInBounds(move.toPos)
+                        && (this.getPos(move.toPos) == null
+                        || this.getPos(move.toPos).color != playerColor)).toList());
+                return moves;
             case QUEEN:
                 break;
             case KING:
-                break;
+                possibleMoves = new ArrayList<>(12);
+
+                possibleMoves.add(new Move(fromPos, oneStepForward(fromPos, playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepBackward(fromPos, playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepLeftAndForward(fromPos, playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepLeftAndBackward(fromPos, playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepRightAndForward(fromPos, playerColor)));
+                possibleMoves.add(new Move(fromPos, oneStepRightAndBackward(fromPos, playerColor)));
+                possibleMoves.add(new Move(fromPos, bishopStepLeft(fromPos, playerColor)));
+                possibleMoves.add(new Move(fromPos, bishopStepRight(fromPos, playerColor)));
+                possibleMoves.add(new Move(fromPos, bishopStepBackwardLeft(fromPos, playerColor)));
+                possibleMoves.add(new Move(fromPos, bishopStepForwardLeft(fromPos, playerColor)));
+                possibleMoves.add(new Move(fromPos, bishopStepBackwardRight(fromPos, playerColor)));
+                possibleMoves.add(new Move(fromPos, bishopStepForwardRight(fromPos, playerColor)));
+
+                return new ArrayList<>(possibleMoves.stream().filter(move -> isInBounds(move.toPos)
+                                                                     && (this.getPos(move.toPos) == null
+                                                                     || this.getPos(move.toPos).color != playerColor)).toList());
         }
 
-        // TODO: REMOVE
-        return false;
+        return moves;
     }
 }
--- src/main/model/Game.java
+++
@@ -1,9 +0,0 @@
-public class Game {
-    Piece.Color currentPlayer;
-    Board board;
-
-    public Game() {
-        this.board = new Board();
-        this.currentPlayer = Piece.Color.WHITE;
-    }
-}
