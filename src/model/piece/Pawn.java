b7224cabb0dfe3cff54c19715775e713f3672e4f
Adnan Hasnat <u7300623@anu.edu.au>
2026-06-23 16:35:53 +1000

    started working on adjusting pawn logic for 6 player mode

--- src/model/piece/Pawn.java
+++ src/model/piece/Pawn.java
@@ -5,13 +5,71 @@ import model.Move;
 import model.PieceType;
 import model.Position;
 
-import java.util.HashSet;
+import java.util.*;
 import java.util.Set;
-import java.util.Set;
+import java.util.function.BiFunction;
 
 public class Pawn extends Piece {
     public Pawn(Color color) {
         super(color);
+    }
+
+    static List<BiFunction<Position, Integer, Position>> movementDirections = List.of(
+            Position::oneStepForward,
+            Position::oneStepRightAndForward,
+            Position::oneStepRightAndBackward,
+            Position::oneStepBackward,
+            Position::oneStepLeftAndBackward,
+            Position::oneStepLeftAndForward
+    );
+
+    enum Direction {
+        FORWARD,
+        CAPTURE_LEFT,
+        CAPTURE_RIGHT,
+        PASSANT_LEFT,
+        PASSANT_RIGHT
+    }
+
+    // helper for getLegalMoves; returns a map of functions corresponding to each direction relevant to the pawn
+    Map<Direction, BiFunction<Position, Integer, Position>> getDirections(Color color) {
+        int forwardDirIndex = switch(color) {
+            case WHITE -> 0;
+            case YELLOW -> 1;
+            case RED -> 2;
+            case BLACK, GREEN -> 3;
+            case BLUE -> 4;
+            case PURPLE -> 5;
+        };
+
+        Map<Direction, BiFunction<Position, Integer, Position>> result = new HashMap<>();
+
+        for (int offset = -2; offset <= 2; offset++) {
+            int index = (forwardDirIndex + offset + 6) % 6;
+
+            Direction dir = switch(offset) {
+                case -2 -> Direction.PASSANT_LEFT;
+                case -1 -> Direction.CAPTURE_LEFT;
+                case 0 -> Direction.FORWARD;
+                case 1 -> Direction.CAPTURE_RIGHT;
+                case 2 -> Direction.PASSANT_RIGHT;
+                default -> throw new IllegalStateException("Unexpected value: " + offset);
+            };
+
+            result.put(dir, movementDirections.get(index));
+        }
+
+        return result;
+    }
+
+    public boolean isStartingPosition(Position pos, Piece.Color color, int boarddim) {
+        if (color == Color.WHITE && boarddim == 6) {
+            return pos.rank == (4 - Position.distanceFromCenter(pos, 2*boarddim-1));
+        } else if (color == Color.BLACK && boarddim == 6) {
+            return pos.rank == 6;
+        }
+        // TODO
+        return false;
     }
 
     @Override
@@ -23,18 +81,15 @@ public class Pawn extends Piece {
     public Set<Move> getMovesFromPos(Board board, Position fromPos) {
         Set<Move> moves = new HashSet<>();
 
+        Piece pawn = board.getPos(fromPos);
+
+        Map<Direction, BiFunction<Position, Integer, Position>> directions = getDirections(pawn.color);
+
         Position forwardPos, doubleForwardPos, leftCapturePos, rightCapturePos;
-        if (this.color == Color.WHITE) {
-            forwardPos = Position.oneStepForward(fromPos, board.getBoardDiameter());
-            doubleForwardPos = Position.oneStepForward(forwardPos, board.getBoardDiameter());
-            leftCapturePos = Position.oneStepLeftAndForward(fromPos, board.getBoardDiameter());
-            rightCapturePos = Position.oneStepRightAndForward(fromPos, board.getBoardDiameter());
-        } else {
-            forwardPos = Position.oneStepBackward(fromPos, board.getBoardDiameter());
-            doubleForwardPos = Position.oneStepBackward(forwardPos, board.getBoardDiameter());
-            leftCapturePos = Position.oneStepLeftAndBackward(fromPos, board.getBoardDiameter());
-            rightCapturePos = Position.oneStepRightAndBackward(fromPos, board.getBoardDiameter());
-        }
+        forwardPos = directions.get(Direction.FORWARD).apply(fromPos, board.getBoardDiameter());
+        doubleForwardPos = directions.get(Direction.FORWARD).apply(forwardPos, board.getBoardDiameter());
+        leftCapturePos = directions.get(Direction.CAPTURE_LEFT).apply(fromPos, board.getBoardDiameter());
+        rightCapturePos = directions.get(Direction.CAPTURE_RIGHT).apply(fromPos, board.getBoardDiameter());
 
         // Pawn basic logic: able to move one space ahead into an empty square
         if (board.isInBounds(forwardPos) && board.getPos(forwardPos) == null)
@@ -53,26 +108,20 @@ public class Pawn extends Piece {
 
         // Pawn logic: if the piece is on a starting position, it can jump two steps forward
         // Note this is true also if the pawn moves into the starting position of another pawn
-        if (this.color == Color.WHITE) {
-            if (fromPos.rank == 4 - Position.distanceFromCenter(fromPos, board.getBoardDiameter())) {
-                if (board.isInBounds(doubleForwardPos) && board.getPos(forwardPos) == null && board.getPos(doubleForwardPos) == null)
-                    moves.add(new Move(fromPos, doubleForwardPos));
-            }
-        } else { // piece is black
-            if (fromPos.rank == 6) {
-                if (board.isInBounds(doubleForwardPos) && board.getPos(forwardPos) == null && board.getPos(doubleForwardPos) == null)
-                    moves.add(new Move(fromPos, doubleForwardPos));
-            }
+        if (isStartingPosition(fromPos, pawn.color, board.getBoardDim())) {
+            if (board.isInBounds(doubleForwardPos) && board.getPos(forwardPos) == null && board.getPos(doubleForwardPos) == null)
+                moves.add(new Move(fromPos, doubleForwardPos));
         }
 
         // Pawn logic: en passant
         // If the enemy pawn has moved 2 spaces in the immediate last turn, and you are attacking the space that it
         // would have moved to if it moved 1 space, then you can still capture it
+        // TODO: fix for 3 and 6 player
         if (board.passantablePawn != null) {
             Position leftPassantPosition, rightPassantPosition;
             if (this.color == Color.WHITE) {
-                leftPassantPosition = Position.oneStepLeftAndBackward(fromPos, board.getBoardDiameter());
-                rightPassantPosition = Position.oneStepRightAndBackward(fromPos, board.getBoardDiameter());
+                leftPassantPosition = directions.get(Direction.PASSANT_LEFT).apply(fromPos, board.getBoardDiameter());
+                rightPassantPosition = directions.get(Direction.PASSANT_RIGHT).apply(fromPos, board.getBoardDiameter());
             } else {
                 leftPassantPosition = Position.oneStepLeftAndForward(fromPos, board.getBoardDiameter());
                 rightPassantPosition = Position.oneStepRightAndForward(fromPos, board.getBoardDiameter());
@@ -92,14 +141,12 @@ public class Pawn extends Piece {
     public Set<Move> getPotentialCapturingMovesFromPos(Board board, Position fromPos) {
         Set<Move> moves = new HashSet<>();
 
+        Piece pawn = board.getPos(fromPos);
+        Map<Direction, BiFunction<Position, Integer, Position>> directions = getDirections(pawn.color);
+
         Position leftCapturePos, rightCapturePos;
-        if (this.color == Color.WHITE) {
-            leftCapturePos = Position.oneStepLeftAndForward(fromPos, board.getBoardDiameter());
-            rightCapturePos = Position.oneStepRightAndForward(fromPos, board.getBoardDiameter());
-        } else {
-            leftCapturePos = Position.oneStepLeftAndBackward(fromPos, board.getBoardDiameter());
-            rightCapturePos = Position.oneStepRightAndBackward(fromPos, board.getBoardDiameter());
-        }
+        leftCapturePos = directions.get(Direction.CAPTURE_LEFT).apply(fromPos, board.getBoardDiameter());
+        rightCapturePos = directions.get(Direction.CAPTURE_RIGHT).apply(fromPos, board.getBoardDiameter());
 
         if (board.isInBounds(leftCapturePos))
             moves.add(new Move(fromPos, leftCapturePos));
